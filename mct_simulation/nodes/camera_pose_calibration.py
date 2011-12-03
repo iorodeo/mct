0564708d64d6fdd9345333a56bcc436417bac328
Peter Polidoro <peterpolidoro@gmail.com>
2011-12-02 19:02:47 -0800

    Render object is now held stationary for some number of rendered frames before changing position and orientation.

--- mct_simulation/nodes/camera_pose_calibration.py
+++ mct_simulation/nodes/camera_pose_calibration.py
@@ -14,13 +14,11 @@ from mct_simulation.find_simulated_camera_images import find_simulated_camera_im
 if __name__ == "__main__":
     env = os.environ.copy()
     simulated_camera_images = find_simulated_camera_images()
+    camera_names = [find_camera_name(simulated_camera_image) for simulated_camera_image in simulated_camera_images]
     proc_list = []
-    for simulated_camera_image in simulated_camera_images:
-        camera_name = find_camera_name(simulated_camera_image)
-        print camera_name
-        # proc_list.append(subprocess.Popen(['rosrun','camera_calibration','cameracalibrator.py','--size 7x5','--square 5.0','image:=' + simulated_camera_image,'camera:=' + camera_name,'&'],env=env))
-        # args_str = 'rosrun camera_calibration cameracalibrator.py --size 7x5 --square 5.0 image:=' + simulated_camera_image + ' camera:=' + camera_name + ' &'
-        # proc_list.append(subprocess.Popen(args_str,shell=True,env=env))
+    if len(camera_names) == 2:
+        args_str = 'roslaunch camera_pose_calibration calibrate_2_camera.launch camera1_ns:=/sim_camera6/camera camera2_ns:=/sim_camera1/camera checker_rows:=7 checker_cols:=5 checker_size:=0.127'
+        proc_list.append(subprocess.Popen(args_str,shell=True,env=env))
 
     raw_input('Press Enter to exit.')
     for proc in proc_list:
--- mct_simulation/nodes/camera_trigger_render_tf_broadcaster.py
+++ mct_simulation/nodes/camera_trigger_render_tf_broadcaster.py
@@ -19,7 +19,14 @@ class RenderTfBroadcaster:
 
     self.camera_name = rospy.get_param("~camera_name")
 
-    self.rate = rospy.Rate(1)
+    self.dev_small = True
+
+    self.trigger_rate = rospy.Rate(2)
+    if self.dev_small:
+      self.trigger_render_ratio = 200
+    else:
+      self.trigger_render_ratio = 20
+    self.trigger_count = 0
 
     # CAD Model
     self.cad_model = CadModel()
@@ -37,6 +44,7 @@ class RenderTfBroadcaster:
     self.render_tf_br = tf.TransformBroadcaster()
     self.camera_trigger_pub = rospy.Publisher('/camera/trigger', String)
 
+    self.tf_initialized = False
     self.initialized = True
 
   def find_position_orientation(self):
@@ -47,19 +55,32 @@ class RenderTfBroadcaster:
     camera_dir /= camera_dist
     camera_angle = min([self.camera_angle,7*math.pi/8])
 
-    dist_min = max(self.calibration_checkerboard_size)/4
+    if self.dev_small:
+      dist_min = max(self.calibration_checkerboard_size)
+    else:
+      dist_min = max(self.calibration_checkerboard_size)/4
     dist_max = camera_dist
 
     dist = random.uniform(dist_min,dist_max)
 
     position = camera_position + camera_dir*dist
+
     dev = dist*math.tan(camera_angle/2)
-    dev /= 2
+    if self.dev_small:
+      dev /= 8
+    else:
+      dev /= 2
+
     position[0] += random.uniform(-dev,dev)
     position[1] += random.uniform(-dev,dev)
 
-    angle_min = -45*math.pi/180
-    angle_max = 45*math.pi/180
+    if self.dev_small:
+      angle_min = -15*math.pi/180
+      angle_max = 15*math.pi/180
+    else:
+      angle_min = -45*math.pi/180
+      angle_max = 45*math.pi/180
+
     ang_x = random.uniform(angle_min,angle_max)
     ang_y = random.uniform(angle_min,angle_max)
     orientation = tf.transformations.quaternion_from_euler(ang_x,ang_y, 0)
@@ -72,18 +93,24 @@ class RenderTfBroadcaster:
   def publish(self):
     while not rospy.is_shutdown():
       if self.initialized:
-        position,orientation = self.find_position_orientation()
-        self.render_tf_br.sendTransform(position,
-                                        orientation,
-                                        rospy.Time.now(),
-                                        "render_object",
-                                        "world")
+        if (self.trigger_count < self.trigger_render_ratio) and self.tf_initialized:
+          self.trigger_count += 1
+        else:
+          if not self.tf_initialized:
+            self.tf_initialized = True
+          self.trigger_count = 0
+          position,orientation = self.find_position_orientation()
+          self.render_tf_br.sendTransform(position,
+                                          orientation,
+                                          rospy.Time.now(),
+                                          "render_object",
+                                          "world")
         self.camera_trigger_pub.publish(String(""))
-        self.rate.sleep()
+        self.trigger_rate.sleep()
 
 
 if __name__ == '__main__':
-  rospy.init_node('render_tf_broadcaster')
+  rospy.init_node('camera_trigger_render_tf_broadcaster')
   rtb = RenderTfBroadcaster()
   try:
     rtb.publish()
