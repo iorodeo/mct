f0b3132579f4c14d3cded749464e41695246faaa
Will Dickson <will@iorodeo.com>
2012-02-12 11:56:41 -0800

    Finished cameracalibrator_master node.

--- mct_camera_calibrator/nodes/cameracalibrator_master.py
+++ mct_camera_calibrator/nodes/cameracalibrator_master.py
@@ -1,8 +1,9 @@
+#!/usr/bin/env python
 from __future__ import print_function
 import roslib 
-PKG = 'mct_camera_calibrator' 
-roslib.load_manifest(PKG)
+roslib.load_manifest('mct_camera_calibrator')
 import rospy
+import subprocess
 
 from mct_msg_and_srv.srv import CameraCalibratorCmd
 from mct_msg_and_srv.srv import CameraCalibratorCmdResponse
@@ -10,11 +11,12 @@ from mct_msg_and_srv.srv import CameraCalibratorCmdResponse
 class CameraCalibratorMaster(object):
 
     def __init__(self):
+        self.calibrator_popen = None
         rospy.on_shutdown(self.clean_up)
         rospy.init_node('cameracalibrator_master')
         self.camera_srv = rospy.Service(
-                'cameracalibrator_master',
-                CommandString,
+                'cameracalibrator_control',
+                CameraCalibratorCmd,
                 self.handle_calibrator_srv,
                 )
 
@@ -22,23 +24,58 @@ class CameraCalibratorMaster(object):
         flag = True
         message = ''
         command = req.command.lower()
-        if req == 'start':
+        if command == 'start':
             # Start calibration node
-            pass
-        elif req == 'stop':
+            if self.calibrator_popen is None:
+                self.start_calibrator_node(req)
+            else:
+                flag = False
+                message = 'calibrator node already running'
+        elif command == 'stop':
             # Stop calilbration node
-            pass
+            if self.calibrator_popen is not None:
+                self.kill_calibrator_node()
+            else:
+                flag = False
+                message = 'calibrator node is not running'
         else:
             flag = False
-            message = 'unknown command string {0}'.format(cmd)
+            message = 'unknown command string {0}'.format(command)
         return CameraCalibratorCmdResponse(flag,message)
+
+    def start_calibrator_node(self,req):
+        """
+        Start camera calibrator node as subprocess.
+        """
+        #print('starting camera calibrator node')
+        popen_list = [
+                'rosrun',
+                'mct_camera_calibrator',
+                'cameracalibrator.py',
+                '--size',
+                '{0}'.format(req.size),
+                '--square',
+                '{0}'.format(req.square),
+                'camera:={0}'.format(req.camera),
+                'image:={0}'.format(req.image),
+                ]
+        #print('popen list: {0}'.format(popen_list))
+        self.calibrator_popen = subprocess.Popen(popen_list) 
+
+    def kill_calibrator_node(self):
+        """
+        Kill camera calibrator node subprocess.
+        """
+        #print('killing camera calibrator node')
+        self.calibrator_popen.send_signal(subprocess.signal.SIGINT)
+        self.calibrator_popen = None
 
     def run(self):
         rospy.spin()
 
     def clean_up(self):
-        pass
-
+        if self.calibrator_popen is not None:
+            self.kill_calibrator_node()
 
 # -----------------------------------------------------------------------------
 if __name__ == '__main__':
---
+++ mct_camera_calibrator/nodes/test_cameracalibrator_master.py
@@ -0,0 +1,34 @@
+from __future__ import print_function
+import roslib 
+roslib.load_manifest('mct_camera_calibrator')
+import rospy
+
+from mct_msg_and_srv.srv import CameraCalibratorCmd
+
+def cameracalibrator_control(command,camera,image,size,square):
+    rospy.wait_for_service('cameracalibrator_control')
+    proxy = rospy.ServiceProxy('cameracalibrator_control',CameraCalibratorCmd)
+    try:
+        response = proxy(command,camera,image,size,square)
+    except rospy.ServiceException, e:
+        print('ERROR: service request failed')
+        response = None
+    return response
+
+# -----------------------------------------------------------------------------
+if __name__ == '__main__':
+    import sys
+
+    cmd = sys.argv[1].lower()
+    if not cmd in ('start','stop'):
+        print('ERROR: command must be start or stop')
+        sys.exit(0)
+
+    command = cmd 
+    camera = '/mct_slave2/camera_10/camera'
+    image = '/mct_slave2/camera_10/camera/image_raw'
+    size = '8x6'
+    square = '0.0254'
+    response = cameracalibrator_control(command,camera,image,size,square)
+    print('response: {0}'.format(response))
+        
