91d268a80bf04207d66837486640dba7cf3d49c0
Will Dickson <will@iorodeo.com>
2012-03-07 18:17:10 -0800

    Added BlobData message type. Modified blob_finder_node so that blob data in published on the appropriate topic.

--- mct_blob_finder/nodes/blob_finder_node.py
+++ mct_blob_finder/nodes/blob_finder_node.py
@@ -13,6 +13,7 @@ from mct_blob_finder import cvblob
 
 # Messages
 from sensor_msgs.msg import Image
+from mct_msg_and_srv.msg import BlobData
 
 class BlobFinderNode(object):
 
@@ -21,8 +22,6 @@ class BlobFinderNode(object):
         self.lock = threading.Lock()
         self.bridge = CvBridge()
         self.threshold = 100
-        self.blob_min_area = 0
-        self.blob_max_area = 100
 
         self.blob_mask  = cvblob.CV_BLOB_RENDER_COLOR 
         self.blob_mask |= cvblob.CV_BLOB_RENDER_CENTROID 
@@ -31,28 +30,40 @@ class BlobFinderNode(object):
         rospy.init_node('blob_finder')
 
         self.image_sub = rospy.Subscriber(self.topic,Image,self.image_handler)
-        self.label_pub = rospy.Publisher('image_blobs', Image)
+        self.image_pub = rospy.Publisher('image_blobs', Image)
+        self.blob_data_pub = rospy.Publisher('blob_data', BlobData)
 
     def image_handler(self,data):
+
         cv_image = self.bridge.imgmsg_to_cv(data,desired_encoding="passthrough")
         raw_image = cv.GetImage(cv_image)
         cv.Threshold(raw_image, raw_image, self.threshold, 255, cv.CV_THRESH_BINARY)
         label_image = cv.CreateImage(cv.GetSize(raw_image), cvblob.IPL_DEPTH_LABEL, 1)
-
         blobs = cvblob.Blobs()
         result = cvblob.Label(raw_image, label_image, blobs)
-        cvblob.FilterByArea(blobs,self.blob_min_area, self.blob_max_area)
-        numblobs = len(blobs.keys())
-
-        print('num: {0}'.format(numblobs))
 
         blob_image = cv.CreateImage(cv.GetSize(raw_image), cv.IPL_DEPTH_8U, 3)
         cv.Zero(blob_image)
         cvblob.RenderBlobs(label_image, blobs, raw_image, blob_image, self.blob_mask, 1.0)
 
         rosimage = self.bridge.cv_to_imgmsg(blob_image,encoding="passthrough")
-        self.label_pub.publish(rosimage)
+        self.image_pub.publish(rosimage)
 
+        blob_data = BlobData()
+        blob_data.header = data.header
+        blob_data.number_of_blobs = len(blobs.keys())
+        for k in blobs:
+            centroid = cvblob.Centroid(blobs[k])
+            blob_data.centroid_x.append(centroid[0])
+            blob_data.centroid_y.append(centroid[1])
+            angle = cvblob.Angle(blobs[k])
+            blob_data.angle.append(angle)
+            blob_data.area.append(blobs[k].area)
+            blob_data.min_x.append(blobs[k].minx)
+            blob_data.max_x.append(blobs[k].maxx)
+            blob_data.min_y.append(blobs[k].miny)
+            blob_data.max_y.append(blobs[k].maxy)
+        self.blob_data_pub.publish(blob_data)
 
     def run(self):
         rospy.spin()
