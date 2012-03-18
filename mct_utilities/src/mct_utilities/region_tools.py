252b8885d4874992f232fe4e486669e673639bc5
Will Dickson <will@iorodeo.com>
2012-03-18 15:11:45 -0700

    Modified camera pairs checker so that regions with a single camera are handled correctly.

--- mct_utilities/src/mct_utilities/region_tools.py
+++ mct_utilities/src/mct_utilities/region_tools.py
@@ -39,25 +39,30 @@ def check_camera_pairs(region_cameras, camera_pairs):
 
     # Check if the set of cameras in the region is equal the set of camers in the
     # pairs list - if not then the pairs don't define a spanning tree for the region
-    pairs_set = set()
-    for camera_0, camera_1 in camera_pairs:
-        pairs_set.add(camera_0)
-        pairs_set.add(camera_1)
     region_set = set(region_cameras)
-    if not (pairs_set == region_set):
-        raise ValueError, 'camera pairs to not define a spanning tree for the region'
+    if len(region_set) > 1:
+        pairs_set = set()
+        for camera_0, camera_1 in camera_pairs:
+            pairs_set.add(camera_0)
+            pairs_set.add(camera_1)
+        if not (pairs_set == region_set):
+            raise ValueError, 'camera pairs to not define a spanning tree for the region'
+    else:
+        if camera_pairs:
+            raise ValueError, 'single camera region w/ camera pair defined'
 
     # Create graph based on region cameras and camera pairs and check that the graph
     # is a tree - i.e., no cycles
-    pairs_graph = nx.Graph()
-    pairs_graph.add_nodes_from(region_cameras)
-    pairs_graph.add_edges_from(camera_pairs)
-    cycles = nx.cycle_basis(pairs_graph,region_cameras[0])
-    if cycles:
-        raise ValueError, 'graph defined by camera pairs is not a tree'
-    components = nx.connected_components(pairs_graph)
-    if len(components) != 1:
-        raise ValueError, 'graph defined by camea pairs has multiple components'
+    if len(region_set) > 1:
+        pairs_graph = nx.Graph()
+        pairs_graph.add_nodes_from(region_cameras)
+        pairs_graph.add_edges_from(camera_pairs)
+        cycles = nx.cycle_basis(pairs_graph,region_cameras[0])
+        if cycles:
+            raise ValueError, 'graph defined by camera pairs is not a tree'
+        components = nx.connected_components(pairs_graph)
+        if len(components) != 1:
+            raise ValueError, 'graph defined by camea pairs has multiple components'
 
 # -----------------------------------------------------------------------------
 if __name__ ==  '__main__':
