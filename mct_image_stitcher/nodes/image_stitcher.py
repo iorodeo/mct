635cde93962e28bb4a0cc46ec1cbb70d85ff7d1c
Will Dickson <will@iorodeo.com>
2012-03-22 13:57:16 -0700

    Added publication of time stamp info for synchronized images - include max, min and diff. Modified the image_stitcher so that the image waiting pool,  stamp to seq pool and  seq to image list buffers are cleaned of any residual data beyond a certain age.

--- mct_image_stitcher/nodes/image_stitcher.py
+++ mct_image_stitcher/nodes/image_stitcher.py
@@ -19,52 +19,47 @@ from mct_transform_2d import transform_2d
 # Messages
 from sensor_msgs.msg import CameraInfo 
 from sensor_msgs.msg import Image 
+from std_msgs.msg import UInt32
+from mct_msg_and_srv.msg import StampInfo
+
+MAX_UINT32 = 2**32 - 1
 
 class ImageStitcher(object):
+    """
+    Subscribes to all rectified image topics in a 2d tracking region and uses the calibration
+    data to stitch the images together. Publishes image_stitched and image_stitched/seq.
+    """
 
-    def __init__(self, region):
+    def __init__(self, region, max_seq_age=200, max_stamp_age=5.0):
+
         rospy.init_node('image_stitcher')
+        self.max_seq_age = max_seq_age
+        self.max_stamp_age = max_stamp_age
         self.ready = False
+        self.is_first_write = True
+        self.stitched_image = None
+        self.seq_to_images = {}  # Seq to image list buffer
+
+        # Information on the latest sequence received and stitched
+        self.seq_newest = None
+        self.stamp_newest = None
 
         self.lock = threading.Lock()
         self.bridge = CvBridge()
 
-        # Get camera list from region
+        # Get image and camera information 
         regions_dict = file_tools.read_tracking_2d_regions()
         self.region = region
         self.camera_list = regions_dict[region]
         self.create_camera_to_image_dict()
         self.create_camera_to_info_dict()
+
+        # Get transforms from cameras to stitched image and stitched image size
         self.tf2d = transform_2d.Transform2d()
-
-        # Get stitched image size from bounding box
-        bbox = self.tf2d.get_stitching_plane_bounding_box(region)
-        self.image_width = int(math.ceil(bbox['max_x']))
-        self.image_height = int(math.ceil(bbox['max_y']))
-        self.image_size = (self.image_width, self.image_height)
-
-        self.is_first_write = True
-        self.stitched_image = None
-        self.seq_to_images = {}
-        self.scratch_image = {}
-        self.tf_data = {}
-        for camera in self.camera_list:
-            # Get modified transform which maps to scratch image
-            bbox = self.tf2d.get_stitching_plane_bounding_box(region, camera_list=[camera])
-            tf_matrix = self.tf2d.get_camera_to_stitching_plane_tf(camera)
-            roi_x = int(math.floor(bbox['min_x']))
-            roi_y = int(math.floor(bbox['min_y']))
-            roi_w = int(math.ceil(bbox['max_x']) - math.floor(bbox['min_x'] ))
-            roi_h = int(math.ceil(bbox['max_y']) - math.floor(bbox['min_y'] ))
-            shift_matrix = numpy.array([
-                [1.0, 0.0, -bbox['min_x']],
-                [0.0, 1.0, -bbox['min_y']], 
-                [0.0, 0.0, 1.0],
-                ])
-            tf_matrix = numpy.dot(shift_matrix, tf_matrix)
-            self.tf_data[camera] = {'matrix': cv.fromarray(tf_matrix), 'roi': (roi_x, roi_y, roi_w, roi_h)}
+        self.create_tf_data()
+        self.get_stitched_image_size()
             
-        # Pools for incomming data.  
+        # Create pool dictionaries for incomming data.  
         self.stamp_to_seq_pool= {}
         self.image_waiting_pool = {} 
         for camera in self.camera_list:
@@ -83,11 +78,11 @@ class ImageStitcher(object):
             image_handler = functools.partial(self.image_handler, camera)
             self.image_sub[camera] = rospy.Subscriber(topic, Image, image_handler)
 
-        # Stitched image publisher 
+        # Stitched image publisher and seq publisher
         self.image_pub = rospy.Publisher('image_stitched', Image)
-
+        self.seq_pub = rospy.Publisher('image_stitched/seq', UInt32)
+        self.stamp_pub = rospy.Publisher('image_stitched/stamp_info', StampInfo)
         self.ready = True
-
 
     def create_camera_to_image_dict(self):
         """
@@ -113,6 +108,37 @@ class ImageStitcher(object):
                 if camera in topic_split:
                     self.camera_to_info[camera] = topic
 
+    def create_tf_data(self):
+        """
+        Pre-computes transform matrices and roi for creating the stitched images.
+        """
+        self.tf_data = {}
+        for camera in self.camera_list:
+            # Get modified transform which maps to ROI in stitched image 
+            bbox = self.tf2d.get_stitching_plane_bounding_box(region, camera_list=[camera])
+            tf_matrix = self.tf2d.get_camera_to_stitching_plane_tf(camera)
+            roi_x = int(math.floor(bbox['min_x']))
+            roi_y = int(math.floor(bbox['min_y']))
+            roi_w = int(math.ceil(bbox['max_x']) - math.floor(bbox['min_x'] ))
+            roi_h = int(math.ceil(bbox['max_y']) - math.floor(bbox['min_y'] ))
+            shift_matrix = numpy.array([
+                [1.0, 0.0, -bbox['min_x']],
+                [0.0, 1.0, -bbox['min_y']], 
+                [0.0, 0.0, 1.0],
+                ])
+            tf_matrix = numpy.dot(shift_matrix, tf_matrix)
+            self.tf_data[camera] = {'matrix': cv.fromarray(tf_matrix), 'roi': (roi_x, roi_y, roi_w, roi_h)}
+
+    def get_stitched_image_size(self):
+        """
+        Determines the size of the stitched image.
+        """
+        # Get stitched image size from bounding box
+        bbox = self.tf2d.get_stitching_plane_bounding_box(region)
+        self.image_width = int(math.ceil(bbox['max_x']))
+        self.image_height = int(math.ceil(bbox['max_y']))
+        self.image_size = (self.image_width, self.image_height)
+
     def info_handler(self,camera,data):
         """
         Handler for incoming camera info messages. In this callback we place image 
@@ -122,6 +148,18 @@ class ImageStitcher(object):
             with self.lock:
                 stamp = data.header.stamp.secs, data.header.stamp.nsecs
                 self.stamp_to_seq_pool[camera][stamp] = data.header.seq
+
+                # Update newest sequence
+                if self.seq_newest is None:
+                    self.seq_newest = data.header.seq
+                else:
+                    self.seq_newest = max([self.seq_newest, data.header.seq])
+
+                # Update newest time stamp
+                if self.stamp_newest is None:
+                    self.stamp_newest = stamp
+                else:
+                    self.stamp_newest = max([self.stamp_newest, stamp])
 
     def image_handler(self, camera, data):
         """
@@ -140,14 +178,13 @@ class ImageStitcher(object):
     def process_waiting_images(self):
         """ 
         Processes waiting images. Associates images in the waiting pool with their acquisition
-        sequence number and places them in the seq_to_images buffer. Checks to see if the 
-        sequences to images buffer contains all required frames and if so produces and publishes a
-        stitched image.
+        sequence number and places them in the seq_to_images buffer.         
         """
         with self.lock:
             # Associate images in the waiting pool with their seq numbers 
             for camera in self.camera_list:
                 for stamp, image_data in self.image_waiting_pool[camera].items():
+
                     try:
                         seq = self.stamp_to_seq_pool[camera][stamp]
                         del self.stamp_to_seq_pool[camera][stamp]
@@ -159,7 +196,44 @@ class ImageStitcher(object):
                             self.seq_to_images[seq] = {camera: image_data}
                     except KeyError:
                         pass
-        
+
+                    # Clear out stale data from image waiting pool
+                    stamp_age = self.get_stamp_age(stamp)
+                    if stamp_age > self.max_stamp_age:
+                        del self.stamp_to_seq_pool[camera][stamp]
+
+                # Clear out any stale data from stamp-to-seq pool
+                for stamp, seq in self.stamp_to_seq_pool[camera].items():
+                    seq_age = self.get_seq_age(seq)
+                    if seq_age > self.max_seq_age: 
+                        del self.stamp_to_seq_pool[camera][stamp]
+
+
+    def get_stamp_age(self, stamp):
+        """
+        Computes the age of a time stamp.
+        """
+        stamp_secs = stamp_tuple_to_secs(stamp)
+        stamp_newest_secs = stamp_tuple_to_secs(self.stamp_newest)
+        return stamp_newest_secs - stamp_secs
+
+
+    def get_seq_age(self, seq):
+        """
+        Compute sequece age - handling 32bit uint rollover of seq counter.  Note, at 
+        30Hz this should roll over for something like 4yrs, but you never know.
+        """
+        age0 = abs(self.seq_newest - seq)
+        age1 = MAX_UINT32 - seq + self.seq_newest + 1
+        return min([age0, age1])
+
+
+    def publish_stitched_image(self):
+        """
+        Checks to see if the sequences to images buffer contains all required frames and if so 
+        produces and publishes a stitched image.
+        """
+
         # Check to see if we have all images for a given sequence number
         for seq, image_dict in self.seq_to_images.items():
 
@@ -168,44 +242,80 @@ class ImageStitcher(object):
 
                 for i, camera in enumerate(self.camera_list):
 
+                    # Convert ros image to opencv image
                     ros_image = image_dict[camera] 
-                    cv_image = self.bridge.imgmsg_to_cv(ros_image,desired_encoding="passthrough")
+                    cv_image = self.bridge.imgmsg_to_cv(
+                            ros_image,
+                            desired_encoding="passthrough"
+                            )
                     ipl_image = cv.GetImage(cv_image)
 
-                    if self.stitched_image is None:
-                        self.stitched_image = cv.CreateImage(self.image_size,ipl_image.depth,ipl_image.channels)
+                    # Get max and min time stamps for stamp_info
+                    stamp = ros_image.header.stamp
+                    if i == 0:
+                        stamp_max = stamp 
+                        stamp_min = stamp
+                    else:
+                        stamp_max = stamp if stamp.to_sec() > stamp_max.to_sec() else stamp_max 
+                        stamp_mim = stamp if stamp.to_sec() < stamp_min.to_sec() else stamp_min 
 
+                    # Create stitced image if it doesn't exist yet
+                    if self.stitched_image is None:
+                        self.stitched_image = cv.CreateImage(
+                                self.image_size,
+                                ipl_image.depth,
+                                ipl_image.channels
+                                )
+
+                    # Set image warping flags - if first fill rest of image with zeros
                     if self.is_first_write:
                         warp_flags = cv.CV_INTER_LINEAR | cv.CV_WARP_FILL_OUTLIERS
                         self.is_first_write = False
                     else:
                         warp_flags = cv.CV_INTER_LINEAR 
 
-                    ### Get transform from camera to stiching plane
-                    #tf_matrix = cv.fromarray(self.tf2d.get_camera_to_stitching_plane_tf(camera))
-                    #cv.WarpPerspective(ipl_image, self.stitched_image, tf_matrix,flags=warp_flags)
-
-                    # Wap into temporary image 
+                    # Wap into stitched image 
                     cv.SetImageROI(self.stitched_image, self.tf_data[camera]['roi'])
-                    cv.WarpPerspective(ipl_image, self.stitched_image, self.tf_data[camera]['matrix'],flags=warp_flags)
+                    cv.WarpPerspective(
+                            ipl_image, 
+                            self.stitched_image, 
+                            self.tf_data[camera]['matrix'],
+                            flags=warp_flags
+                            )
                     cv.ResetImageROI(self.stitched_image)
 
-
+                # Remove data from buffer
                 del self.seq_to_images[seq]
                 print('syncd seq: {0}'.format(seq))
-                print()
 
                 # Convert stitched image to ros image and publish
-                stitched_ros_image = self.bridge.cv_to_imgmsg(self.stitched_image,encoding="passthrough")
+                stitched_ros_image = self.bridge.cv_to_imgmsg(
+                        self.stitched_image,
+                        encoding="passthrough"
+                        )
+                stitched_ros_image.header.seq = seq
                 self.image_pub.publish(stitched_ros_image)
 
+                # Publish seq information and time stamp spread
+                self.seq_pub.publish(seq)
+                stamp_diff = stamp_max.to_sec() - stamp_min.to_sec()
+                self.stamp_pub.publish(stamp_max, stamp_min, stamp_diff)
+
+            # Throw away any stale data in seq to images buffer
+            seq_age = self.get_seq_age(seq)
+            if seq_age > self.max_seq_age:
+                del self.seq_to_images[seq]
 
     def run(self):
         while not rospy.is_shutdown(): 
             self.process_waiting_images() 
-            rospy.sleep(0.01)
-                
+            self.publish_stitched_image()
+            #rospy.sleep(0.01)
 
+
+# -----------------------------------------------------------------------------
+def stamp_tuple_to_secs(stamp):
+    return stamp[0] + stamp[1]*1.0e-9
 
 
 # -----------------------------------------------------------------------------
