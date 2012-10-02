dca6948f94e683009456c9c32cd6d7f6b0b80079
Will Dickson <will@iorodeo.com>
2012-10-02 12:25:40 -0400

    Modified frame_skipper so that it uses the correct manifest.

--- mct_frame_skipper/nodes/frame_skipper_master.py
+++ mct_frame_skipper/nodes/frame_skipper_master.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from __future__ import print_function
 import roslib
-roslib.load_manifest('mct_image_stitcher')
+roslib.load_manifest('mct_frame_skipper')
 import rospy
 
 import os
