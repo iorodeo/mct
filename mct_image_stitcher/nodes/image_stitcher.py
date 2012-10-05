b1831d46fe76e9d5a6867d568fc5f4c89ee286d8
Will Dickson <will@iorodeo.com>
2012-10-04 21:34:23 -0400

    Began modifiying image sticher to handle SeqAndImage topic type produced by the frame drop corrector nodes.

--- mct_image_stitcher/nodes/image_stitcher.py
+++ mct_image_stitcher/nodes/image_stitcher.py
@@ -29,22 +29,29 @@ MAX_UINT32 = 2**32 - 1
 
 class ImageStitcher(object):
     """
-    Subscribes to all rectified image topics in a 2d tracking region and uses the calibration
-    data to stitch the images together. Publishes image_stitched and image_stitched/seq.
+    Subscribes to all rectified image topics in a 2d tracking region and uses
+    the calibration data to stitch the images together. Publishes
+    image_stitched and image_stitched/seq.
     """
 
-    def __init__(self, region, max_seq_age=150, max_stamp_age=1.5):
+    def __init__(
+            self, 
+            region, 
+            topic_end='image_rect_skip', 
+            topic_type='sensor_msgs/Image', 
+            max_seq_age=150, 
+            max_stamp_age=1.5
+            ):
+
+        self.topic_end = topic_end
+        self.topic_type = topic_type
+        self.max_seq_age = max_seq_age
+        self.max_stamp_age = max_stamp_age
 
         self.warp_flags = cv.CV_INTER_LINEAR 
         self.stitching_params = file_tools.read_tracking_2d_stitching_params()
 
-        # Trying new frame skipper nodes #############
-        self.stitching_params['frame_skip'] = 1
-        ##############################################
-
         rospy.init_node('image_stitcher')
-        self.max_seq_age = max_seq_age
-        self.max_stamp_age = max_stamp_age
         self.ready = False
         self.is_first_write = True
         self.stitched_image = None
@@ -68,25 +75,40 @@ class ImageStitcher(object):
         self.tf2d = transform_2d.Transform2d()
         self.create_tf_data()
         self.get_stitched_image_size()
+
+        # Subscribe to topics based on incoming topic type.
+        if self.topic_type == 'sensor_msgs/Image':
             
-        # Create pool dictionaries for incomming data.  
-        self.stamp_to_seq_pool= {}
-        self.image_waiting_pool = {} 
-        for camera in self.camera_list:
-            self.stamp_to_seq_pool[camera] = {}
-            self.image_waiting_pool[camera] = {}
+            # Create pool dictionaries for incomming data.  
+            self.stamp_to_seq_pool= {}
+            self.image_waiting_pool = {} 
+            for camera in self.camera_list:
+                self.stamp_to_seq_pool[camera] = {}
+                self.image_waiting_pool[camera] = {}
 
-        # Subscribe to camera info topics
-        self.info_sub ={} 
-        for camera, topic  in  self.camera_to_info.iteritems():
-            info_handler = functools.partial(self.info_handler, camera)
-            self.info_sub[camera] = rospy.Subscriber(topic, CameraInfo, info_handler)
+            # Subscribe to camera info topics
+            self.info_sub = {} 
+            for camera, topic  in  self.camera_to_info.iteritems():
+                info_handler = functools.partial(self.info_handler, camera)
+                self.info_sub[camera] = rospy.Subscriber(topic, CameraInfo, info_handler)
 
-        # Subscribe to rectified image topics
-        self.image_sub ={} 
-        for camera, topic  in  self.camera_to_image.iteritems():
-            image_handler = functools.partial(self.image_handler, camera)
-            self.image_sub[camera] = rospy.Subscriber(topic, Image, image_handler)
+            # Subscribe to rectified image topics
+            self.image_sub = {} 
+            for camera, topic  in  self.camera_to_image.iteritems():
+                image_handler = functools.partial(self.image_handler, camera)
+                self.image_sub[camera] = rospy.Subscriber(topic, Image, image_handler)
+
+        elif self.topic_type == 'mct_msg_and_srv/SeqAndImage':
+
+            # Subscribe to SeqAndImage topics
+            self.seq_and_image_sub = {}
+            for camera, topic in self.camera_to_image.iteritems():
+                seq_and_image_handler = functools.partial(self.seq_and_image_handler, camera)
+                self.image_sub[camera] = rospy.Subscriber(topic, SeqAndImage, seq_and_image_handler)
+
+        else:
+            err_msg = 'unable to handle topic type: {0}'.format(self.topic_type)
+            raise ValueError, err_msg
 
         # Stitched image publisher and seq publisher
         self.image_pub = rospy.Publisher('image_stitched', Image)
@@ -101,11 +123,13 @@ class ImageStitcher(object):
         Create camera to image topic dictionary
         """
         self.camera_to_image = {}
-        # Trying new frame skipper nodes ####################################################
-        rect_topics = mct_introspection.find_camera_image_topics(transport='image_rect_skip')
-        #####################################################################################
+        image_topics = mct_introspection.find_camera_image_topics(transport=self.topic_end)
+        # ------------------------------------------------------------------------------------
+        #rect_topics = mct_introspection.find_camera_image_topics(transport='image_rect_skip')
         #rect_topics = mct_introspection.find_camera_image_topics(transport='image_rect')
-        for topic in rect_topics:
+        # ------------------------------------------------------------------------------------
+        #for topic in rect_topics:
+        for topic in image_topics:
             topic_split = topic.split('/')
             for camera in self.camera_list:
                 if camera in topic_split:
@@ -191,6 +215,9 @@ class ImageStitcher(object):
                 stamp = data.header.stamp.secs, data.header.stamp.nsecs
                 self.image_waiting_pool[camera][stamp] = data
 
+    def seq_and_image_handler(self,camera,data):
+        pass
+
     def process_waiting_images(self):
         """ 
         Processes waiting images. Associates images in the waiting pool with their acquisition
@@ -258,76 +285,73 @@ class ImageStitcher(object):
             # If we have all images for the sequece - stitch into merged view
             if len(image_dict) == len(self.camera_list):
 
-                # Get start time to measure processing dt
-                if seq%int(self.stitching_params['frame_skip']) == 0:
+                t0 = rospy.Time.now() 
 
-                    t0 = rospy.Time.now() 
+                for i, camera in enumerate(self.camera_list):
 
-                    for i, camera in enumerate(self.camera_list):
-
-                        # Convert ros image to opencv image
-                        ros_image = image_dict[camera] 
-                        cv_image = self.bridge.imgmsg_to_cv(
-                                ros_image,
-                                desired_encoding="passthrough"
-                                )
-                        ipl_image = cv.GetImage(cv_image)
-
-                        # Create stitced image if it doesn't exist yet
-                        if self.stitched_image is None:
-                            self.stitched_image = cv.CreateImage(
-                                    self.image_size,
-                                    ipl_image.depth,
-                                    ipl_image.channels
-                                    )
-                        if i==0:
-                            cv.Zero(self.stitched_image)
-
-                        # Set image warping flags - if first fill rest of image with zeros
-                        if self.is_first_write:
-                            warp_flags = self.warp_flags | cv.CV_WARP_FILL_OUTLIERS
-                            self.is_first_write = False
-                        else:
-                            warp_flags = self.warp_flags
-
-                        # Warp into stitched image 
-                        cv.SetImageROI(self.stitched_image, self.tf_data[camera]['roi'])
-
-                        # Warp stitched image
-                        cv.WarpPerspective(
-                                ipl_image, 
-                                self.stitched_image, 
-                                self.tf_data[camera]['matrix'],
-                                flags=warp_flags,
-                                )
-                        cv.ResetImageROI(self.stitched_image)
-
-                        # Get max and min time stamps for stamp_info
-                        stamp = ros_image.header.stamp
-                        if i == 0:
-                            stamp_max = stamp 
-                            stamp_min = stamp
-                        else:
-                            stamp_max = stamp if stamp.to_sec() > stamp_max.to_sec() else stamp_max 
-                            stamp_mim = stamp if stamp.to_sec() < stamp_min.to_sec() else stamp_min 
-
-                    # Convert stitched image to ros image
-                    stitched_ros_image = self.bridge.cv_to_imgmsg(
-                            self.stitched_image,
-                            encoding="passthrough"
+                    # Convert ros image to opencv image
+                    ros_image = image_dict[camera] 
+                    cv_image = self.bridge.imgmsg_to_cv(
+                            ros_image,
+                            desired_encoding="passthrough"
                             )
-                    stitched_ros_image.header.seq = seq
-                    self.image_pub.publish(stitched_ros_image)
-                    self.image_and_seq_pub.publish(seq, stitched_ros_image)
-                    self.seq_pub.publish(seq)
+                    ipl_image = cv.GetImage(cv_image)
 
-                    # Compute time stamp spread
-                    stamp_diff = stamp_max.to_sec() - stamp_min.to_sec()
-                    self.stamp_pub.publish(stamp_max, stamp_min, stamp_diff)
-                    
-                    t1 = rospy.Time.now()
-                    dt = t1.to_sec() - t0.to_sec()
-                    self.processing_dt_pub.publish(dt,1.0/dt)
+                    # Create stitced image if it doesn't exist yet
+                    if self.stitched_image is None:
+                        self.stitched_image = cv.CreateImage(
+                                self.image_size,
+                                ipl_image.depth,
+                                ipl_image.channels
+                                )
+                    if i==0:
+                        cv.Zero(self.stitched_image)
+
+                    # Set image warping flags - if first fill rest of image with zeros
+                    if self.is_first_write:
+                        warp_flags = self.warp_flags | cv.CV_WARP_FILL_OUTLIERS
+                        self.is_first_write = False
+                    else:
+                        warp_flags = self.warp_flags
+
+                    # Warp into stitched image 
+                    cv.SetImageROI(self.stitched_image, self.tf_data[camera]['roi'])
+
+                    # Warp stitched image
+                    cv.WarpPerspective(
+                            ipl_image, 
+                            self.stitched_image, 
+                            self.tf_data[camera]['matrix'],
+                            flags=warp_flags,
+                            )
+                    cv.ResetImageROI(self.stitched_image)
+
+                    # Get max and min time stamps for stamp_info
+                    stamp = ros_image.header.stamp
+                    if i == 0:
+                        stamp_max = stamp 
+                        stamp_min = stamp
+                    else:
+                        stamp_max = stamp if stamp.to_sec() > stamp_max.to_sec() else stamp_max 
+                        stamp_mim = stamp if stamp.to_sec() < stamp_min.to_sec() else stamp_min 
+
+                # Convert stitched image to ros image
+                stitched_ros_image = self.bridge.cv_to_imgmsg(
+                        self.stitched_image,
+                        encoding="passthrough"
+                        )
+                stitched_ros_image.header.seq = seq
+                self.image_pub.publish(stitched_ros_image)
+                self.image_and_seq_pub.publish(seq, stitched_ros_image)
+                self.seq_pub.publish(seq)
+
+                # Compute time stamp spread
+                stamp_diff = stamp_max.to_sec() - stamp_min.to_sec()
+                self.stamp_pub.publish(stamp_max, stamp_min, stamp_diff)
+                
+                t1 = rospy.Time.now()
+                dt = t1.to_sec() - t0.to_sec()
+                self.processing_dt_pub.publish(dt,1.0/dt)
 
                 # Remove data from buffer
                 del self.seq_to_images[seq]
